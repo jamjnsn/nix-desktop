e2a8ab97aa3f106bb74c50ff78a30b466783896d
Jamie Jansen <jamie@jnsn.me>
2025-06-30 08:48:39 -0700

    Remove eza theme

--- users/jamie/modules/eza.nix
+++ users/jamie/modules/eza.nix
@@ -1,6 +1,5 @@
 {
   programs.eza.enable = true;
-  home.file.".config/eza/theme.yml".source = ../dotfiles/eza/one-dark.yml;
   programs.zsh.shellAliases = {
     ls = "eza --icons --group-directories-first";
   };
