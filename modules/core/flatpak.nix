141f791c31efbe82e1924de6bfe55268840b4b21
Jamie Jansen <jamie@jnsn.me>
2025-07-07 13:23:01 -0700

    Add some more flatpaks

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -12,6 +12,10 @@
       "org.gnome.World.PikaBackup"
       "com.github.tchx84.Flatseal"
       "it.mijorus.smile"
+      "org.gnome.gitlab.YaLTeR.VideoTrimmer"
+      "io.github.celluloid_player.Celluloid"
+      "com.obsproject.Studio"
+      "org.libreoffice.LibreOffice"
     ];
 
     update.auto = {
