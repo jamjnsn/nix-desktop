bce70b9176582c9d920f4a90a05699ee7bf3cc3f
Jamie Jansen <jamie@jnsn.me>
2025-07-09 14:34:46 -0700

    Remove comment

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -14,7 +14,7 @@
       "wheel"
       "networkmanager"
     ];
-    hashedPassword = "$6$bNA3pkIN8HqDgv2H$s50wORlm48JP/dwzHZAhDU8c5DToBluyCMd3f.IlTnOJ87js6Cw0KS3D40tRNvoslFV8oHBJfk8JNipjVVzvq1"; # empty password
+    hashedPassword = "$6$bNA3pkIN8HqDgv2H$s50wORlm48JP/dwzHZAhDU8c5DToBluyCMd3f.IlTnOJ87js6Cw0KS3D40tRNvoslFV8oHBJfk8JNipjVVzvq1";
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICG/Ktp96ldlqZwoH4dGQl6uBLBF3i8xLbnF4PAS+gJx jamie@desky" # WSL
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICwFbe6YCNc/WHQRnCsrZu6NH8ny585VBrRzMJpB/mmW jamie@lappy"
