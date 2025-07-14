adcec6f1b88e5ba60b6a1dd726b1c9bd4bc46338
Jamie Jansen <jamie@jnsn.me>
2025-07-13 19:06:24 -0700

    Add micro config

--- modules/home/micro.nix
+++ modules/home/micro.nix
@@ -2,6 +2,42 @@
 {
   programs.micro = {
     enable = true;
+
+    settings = {
+      colorscheme = "transparent";
+      tabsize = 4;
+      softwrap = true;
+      ruler = true;
+    };
+  };
+
+  # Shell alias so I don't forget to use micro
+  programs.zsh.shellAliases = {
+    nano = "micro";
+  };
+
+  xdg.configFile."micro/colorschemes/transparent.micro" = {
+    text = ''
+      color-link default "none,none"
+      color-link comment "blue,none"
+      color-link identifier "cyan,none"
+      color-link constant "magenta,none"
+      color-link statement "yellow,none"
+      color-link type "green,none"
+      color-link preproc "red,none"
+      color-link special "brightmagenta,none"
+      color-link underlined "underline,none"
+      color-link error "brightred,none"
+      color-link todo "brightblue,none"
+      color-link selection "black,white"
+      color-link statusline "white,black"
+      color-link tabbar "white,black"
+      color-link indent-char "brightblack,none"
+      color-link line-number "brightblack,none"
+      color-link current-line-number "white,none"
+      color-link cursor-line "none,none"
+      color-link color-column "none,none"
+    '';
   };
 
   xdg.configFile."micro/bindings.json".text = ''
