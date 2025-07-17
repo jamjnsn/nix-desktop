d9a1932d03577d0963a121ac1c46e1a3ce6350b2
Jamie Jansen <jamie@jnsn.me>
2025-07-16 21:42:35 -0700

    nix-flatpak adds flathub

--- modules/core/flatpak.nix
+++ modules/core/flatpak.nix
@@ -23,12 +23,4 @@
       onCalendar = "daily";
     };
   };
-
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
 }
