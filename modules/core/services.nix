6a524d7146e0992dec4ea68b520f1c96725a7c1f
Jamie Jansen <jamie@jnsn.me>
2025-08-27 08:09:02 -0700

    add some extra services

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -3,6 +3,9 @@
   services = {
     gvfs.enable = true;
     printing.enable = true;
+    locate.enable = true;
+    udisks2.enable = true; # For automounting drives
+    btrfs.autoScrub.enable = true;
 
     fstrim.enable = lib.mkDefault true;
     libinput.enable = true;
