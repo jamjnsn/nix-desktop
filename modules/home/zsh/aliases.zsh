6bfc22e3ae101520c5c8f60ff85a7b0d85c2bf74
Jamie Jansen <jamie@jnsn.me>
2025-10-23 09:15:13 -0700

    set aliases

--- modules/home/zsh/aliases.zsh
+++ modules/home/zsh/aliases.zsh
@@ -4,7 +4,7 @@ alias dc="docker compose"
 
 # Default args
 alias mv="mv -iv"
-alias cp="cp -iv"
+alias cp="cp -iv --reflink=auto" # Use reflink where supported
 alias mkdir="mkdir -pv"
 alias wget="wget -c"
 alias du="du -h"
@@ -14,14 +14,8 @@ alias diff="diff --color"
 # Colorize
 alias ping="grc \\ping"
 
-# Add untracked files in the repo and rebuild the system flake
-alias nrs="(cd ~/.config/nixos && git add -N . && sudo nixos-rebuild switch --flake .)"
-
 # Nix shell with packages (arguments can be empty)
 alias nsh="nix-shell --packages"
-
-# Update Nix packages
-alias update="sudo nix-channel --update"
 
 # For some reason `agenix-cli` doesn't work but this does:
 alias agenix="nix run github:ryantm/agenix --"
